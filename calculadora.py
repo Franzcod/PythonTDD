e54f622972be18be4caa085cdb15c3e170c066f3
Palacios Francisco <53352272+Franzcod@users.noreply.github.com>
2021-01-10 14:29:46 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+class Calculadora:
+
+	total = 0
+
+	def valor(self):
+		return self.total
+
+	def suma(self,uno, dos):
+		self.total = uno + dos
---
+++ test_test.py
@@ -0,0 +1,19 @@
+import unittest
+from calculadora import Calculadora
+
+
+class TestsBasicos(unittest.TestCase):
+
+	def setUp(self):
+		self.calc = Calculadora()
+
+	def test_una_calculadora_nueva_inicia_en_cero(self):
+		self.assertEqual(0, self.calc.valor())
+
+	def test_la_sa_de_2_mas_3_debe_dar_5(self):
+		self.calc.suma(2,3)
+		self.assertEqual(5, self.calc.valor())
+
+	def test_la_sa_de_4_mas_3_debe_dar_7(self):
+		self.calc.suma(4,3)
+		self.assertEqual(7, self.calc.valor())
